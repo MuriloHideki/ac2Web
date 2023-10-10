ddc4c049e9e09044a443510cbf4dd83405f8627b
Murilo <71025135+MuriloHideki@users.noreply.github.com>
2023-10-09 21:59:12 -0300

    Add files via upload

---
+++ Pages/Form/formPg1/formPg1.js
@@ -0,0 +1,32 @@
+var title = "CVV - Form";
+
+function start(){
+    document.getElementById("brand").innerText = title;
+    document.getElementById("card2").style.display = "none";
+    document.getElementById("card3").style.display = "none";
+
+    //TESTE
+    document.getElementById("card1").style.display = "none";
+    document.getElementById("card2").style.display = "flex";
+    document.getElementById("card3").style.display = "none";
+}
+
+function nextForm1(){
+    document.getElementById("card2").style.display = "flex";
+    document.getElementById("card1").style.display = "none";
+}
+
+function nextForm2(){
+    document.getElementById("card3").style.display = "flex";
+    document.getElementById("card2").style.display = "none";
+}
+
+function previousForm2(){
+    document.getElementById("card1").style.display = "flex";
+    document.getElementById("card2").style.display = "none";
+}
+
+function previousForm3(){
+    document.getElementById("card2").style.display = "flex";
+    document.getElementById("card3").style.display = "none";
+}
---
+++ Pages/Home/home.js
@@ -0,0 +1,3 @@
+function start(){
+    document.getElementById("brand").textContent = "CVV";
+}
